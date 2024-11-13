42410423419a24ad4d43ff6e52520d19a2addc04
gersh0m <gersh0m@lila-erc.eu>
2024-11-13 11:46:43 +0100

    BUGFIX absolute

---
+++ mod/BUGFIX_absolute.sql
@@ -0,0 +1,11 @@
+/*
+
+Questo lemma della Lemma Bank ("absolute", NOUN) va deprecato: http://lila-erc.eu/data/id/lemma/26407.
+Il deprecante è: http://lila-erc.eu/data/id/hypolemma/113033
+
+Va rimosso anche da LEMLAT, dove ha id_lemma='70809' e la sua clem (da rimuovere) è D01FF.
+*/
+
+CALL SP_deprecateClem('D01FF','a0178');
+
+
